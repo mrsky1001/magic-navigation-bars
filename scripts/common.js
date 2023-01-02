3922b80765247b1d53f70f6bc9f0e7daeac88637
mrsky1001 <nikita.nk16@yandex.ru>
2023-01-02 21:50:52 +0600

    init

---
+++ scripts/common.js
@@ -0,0 +1,20 @@
+const setListenersForActiveTabs = (queryString) => {
+    const list = document.querySelectorAll(queryString)
+
+    const activeLink = (t) => {
+        list.forEach((item) => {
+            item.classList.remove('active');
+            t.classList.add('active');
+        })
+    }
+
+    list.forEach(item => {
+        item.addEventListener('click', () => activeLink(item))
+    })
+}
+
+
+setListenersForActiveTabs('.bubble-up.navigation > ul > li.list')
+setListenersForActiveTabs('.bubble-down.navigation  > ul > li.list')
+setListenersForActiveTabs('.convex-up.navigation  > ul > li.list')
+setListenersForActiveTabs('.convex-down.navigation  > ul > li.list')
